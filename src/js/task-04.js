3946374e69819fed417cac9de6fec6d35fede2a8
YaroslavaSenko <yaroslava_yatskevych@ukr.net>
2022-12-04 16:04:33 +0200

    Update task-04.js

--- src/js/task-04.js
+++ src/js/task-04.js
@@ -1,1 +1,16 @@
-const counter = document.querySelectorAll('#counter')
+let counterValue = 0
+
+const counter = document.querySelector('#value')
+const minus = document.querySelector('button[data-action="decrement"]')
+const plus = document.querySelector('button[data-action="increment"]')
+
+minus.addEventListener('click', () => {
+ counterValue--
+ counter.textContent = counterValue
+})
+
+plus.addEventListener('click', () => {
+ counterValue++
+ counter.textContent = counterValue
+
+})
