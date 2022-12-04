67ee82aa60c06d63f98c66c103df4ba1d1ceb22d
YaroslavaSenko <yaroslava_yatskevych@ukr.net>
2022-12-04 16:13:15 +0200

    deleted files

--- src/js/task-01.js
+++
@@ -1,8 +0,0 @@
-const itemList = document.querySelectorAll('.item');
-console.log('Number of categories:', itemList.length)
-
-itemList.forEach(item => {
-    console.log('Category:', item.firstElementChild.textContent)
-    console.log('Elements:', item.lastElementChild.children.length)
-});
-
